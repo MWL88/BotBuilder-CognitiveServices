2e557c1c4f375956d9e892c7f494639c5376bde7
Mark Leach <mwleach@hotmail.com>
2017-05-29 16:44:01 +0100

    Add nobuild option

--- CSharp/Library/createpackage.cmd
+++ CSharp/Library/createpackage.cmd
@@ -5,7 +5,7 @@ setlocal enableextensions
 set errorlevel=0
 mkdir ..\nuget
 erase /s ..\nuget\Microsoft.Bot.CognitiveServices.QnAMaker*nupkg
-msbuild /property:Configuration=release Microsoft.Bot.Builder.CognitiveServices.QnAMaker.csproj 
+IF NOT {%1}=={nobuild} msbuild /property:Configuration=release Microsoft.Bot.Builder.CognitiveServices.QnAMaker.csproj 
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.CognitiveServices.QnAMaker.dll).FileVersionInfo.FileVersion"') do set version=%%v
 ..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.CognitiveServices.QnAMaker.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
