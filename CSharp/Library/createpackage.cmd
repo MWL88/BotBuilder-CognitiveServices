91712a1275019b380716b51f0c1db2f4a4b8b041
Mark Leach <mwleach@hotmail.com>
2017-05-29 16:50:13 +0100

    Update packaging script

--- CSharp/Library/createpackage.cmd
+++ CSharp/Library/createpackage.cmd
@@ -8,4 +8,4 @@ erase /s ..\nuget\Microsoft.Bot.CognitiveServices.QnAMaker*nupkg
 IF NOT {%1}=={nobuild} msbuild /property:Configuration=release Microsoft.Bot.Builder.CognitiveServices.QnAMaker.csproj 
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.CognitiveServices.QnAMaker.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.CognitiveServices.QnAMaker.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
+IF NOT {%1}=={nobuild} ..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.CognitiveServices.QnAMaker.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
