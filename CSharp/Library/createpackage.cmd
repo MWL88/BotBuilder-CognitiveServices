f75ac162b9963c986768f64105ae4844566c06da
Mark Leach <mwleach@hotmail.com>
2017-05-29 17:30:07 +0100

    Update batch vars

--- CSharp/Library/createpackage.cmd
+++ CSharp/Library/createpackage.cmd
@@ -11,3 +11,6 @@ IF NOT {%1}=={nobuild} msbuild /property:Configuration=release Microsoft.Bot.Bui
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.CognitiveServices.QnAMaker.dll).FileVersionInfo.FileVersion"') do set version=%%v
 IF NOT {%1}=={nobuild} ..\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe pack Microsoft.Bot.Builder.CognitiveServices.QnAMaker.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
+
+ IF {%1}=={nobuild} echo ##vso[task.setvariable variable=builder;]%builder%
+ IF {%1}=={nobuild} echo ##vso[task.setvariable variable=version;]%version% 
